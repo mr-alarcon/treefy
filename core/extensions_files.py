f00f13bc48ba3804ad6100cf4cde60b8ae2bd50b
mr-alarcon <marcosdavidalarcon06@gmail.com>
2025-08-15 15:30:02 -0400

    Add core folder and create extensions_files_list module

---
+++ core/extensions_files.py
@@ -0,0 +1,18 @@
+def create_extensions_dict():
+    exts = [
+        "php", "asp", "aspx", "jsp", "cgi", "pl", "py", "rb",
+        "js", "json", "xml",
+        "env", "config", "ini", "yml", "yaml",
+        "bak", "old", "zip", "rar",
+        "log", "sql", "db",
+        "htaccess", "htpasswd",
+        "pem", "key", "crt", "p12", "pfx",
+        "inc", "tpl", "sh", "exe", "bin", "ps1", "git",
+        "html", "htm", "css",
+        "jpg", "jpeg", "png", "gif", "svg", "webp", "ico",
+        "woff", "woff2", "ttf", "eot",
+        "mp4", "webm", "ogg", "mp3", "wav", "mov",
+        "pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx"
+    ]
+
+    return exts
