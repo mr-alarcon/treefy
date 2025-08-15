7e997171e5cd30a6b7c853f23bbd36b0413cf0cb
mr-alarcon <marcosdavidalarcon06@gmail.com>
2025-08-15 15:31:53 -0400

    Add directory_tree module to build website directory tree from URLs list

---
+++ core/directory_tree.py
@@ -0,0 +1,32 @@
+links = [
+    "example/folder1/files/image.png" 
+]
+
+
+def add_segments_to_tree(current_tree, segments):
+    for segmt in segments:
+        if segmt not in current_tree:
+            current_tree[segmt] = {}
+
+        current_tree = current_tree[segmt]
+
+
+def url_segments(url):
+    if url.startswith("#"):
+        return
+    else:
+        url_seg = url.strip("/").split("/")
+        return url_seg
+
+
+
+def build_directory_tree(links):
+    directory_tree = {}
+
+    for link in links:
+        segments = url_segments(link)
+        add_segments_to_tree(directory_tree, segments)
+
+    return directory_tree
+
+
