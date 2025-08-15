a09e9d36132146e18a1640cc175ebfe0373cba86
mr-alarcon <marcosdavidalarcon06@gmail.com>
2025-08-15 13:42:27 -0400

    Add color styles to the main banner module

--- cli/main_banner.py
+++ cli/main_banner.py
@@ -1,26 +1,32 @@
+from colorama import Fore, Back, Style
+
 def create_main_banner():
-    print("""               
-               ░░░░░░░░░░░░░░░░░░░░
-            ░░░░░░░░░░░░┃░┃░░░◎░░░░░░
-          ░░░░░░░░░░░░░░┃░┃░░░│░░░░░░░░░
-        ░░░░░░░░░░░░░░░░┃░┃┬──┘░░░░░░░░░
-            ░░░░░░░░░░░░┃░┃└──░░░░░░░░░
-            ░░░░░◎░░░░░░┃░┃░░░░◎░░░░░░░░
-           ░░░░░░◎░░░░░░┃░┃░░┼──┘░░░░░░░░
-            ░░░░░░░░░└┐░┃░┃┌┴┴░░░◎░░░░░░░░
-        ░░░░░░░│░░░░░░└─┃░┃┴░░░░░░░░░░▒░░░░░
-       ░░▒░░░│░│░░░░░░░░┃░┃░░░░░◎░░░░░░░░░░
-       ░░░░░░└─┤░░◎░░░│░┃░┃░░░░░░░░░░░░░░░              
-         ░░░░░░└█░│░░░│░▌░▌░░░░░░│░│░░░░░░
-          ░░░░░──█┘░◎░┴─█░█░░░░░░█─┴─░░░░░░
-         ░░└█░░░ █ ░░░░░▀██─┘┌┘░█┘░░░░░▄┴░
-         └───█▄▄▄▄█░┬┘░░░██──┘░░█░░▄▄▄█
-                  █─┘░░░░██▄░░░█  █  
-                   ▀▀▀▀▀████▀▀▀ ▀▀ 
-                        ████            └─┤  mr-alarcon
+    
+    print(f"""               
+               {Fore.GREEN}░░░░░░░░░░░░░░░░░░░░
+            ░░░░░░░░░░░░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░░░{Fore.RED}◎{Fore.GREEN}░░░░░░
+          ░░░░░░░░░░░░░░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░░░{Fore.YELLOW}│{Fore.GREEN}░░░░░░░░░
+        ░░░░░░░░░░░░░░░░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃┬──┘{Fore.GREEN}░░░░░░░░░
+            ░░░░░░░░░░░░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃└──{Fore.GREEN}░░░░░░░░░
+            ░░░░░{Fore.RED}◎{Fore.GREEN}░░░░░░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░░░░{Fore.RED}◎{Fore.GREEN}░░░░░░░░
+            ░░░░░░░░░░░░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░░{Fore.YELLOW}┼──┘{Fore.GREEN}░░░░░░░░
+            ░░░░░░░░░{Fore.YELLOW}└┐{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃┌┴┴{Fore.GREEN}░░░{Fore.RED}◎{Fore.GREEN}░░░░░░░░
+        ░░░░░░░{Fore.YELLOW}│{Fore.GREEN}░░░░░░{Fore.YELLOW}└─┃{Fore.GREEN}░{Fore.YELLOW}┃┴{Fore.GREEN}░░░░░░░░░░▒░░░░░
+       ░░▒░░░{Fore.YELLOW}│{Fore.GREEN}░{Fore.YELLOW}│{Fore.GREEN}░░░░░░░░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░░░░░{Fore.RED}◎{Fore.GREEN}░░░░░░░░░░
+       ░░░░░░{Fore.YELLOW}└─┤{Fore.GREEN}░░{Fore.RED}◎{Fore.GREEN}░░░{Fore.YELLOW}│{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░{Fore.YELLOW}┃{Fore.GREEN}░░░░░░░░░░░░░░░              
+         ░░░░░░{Fore.YELLOW}└█{Fore.GREEN}░{Fore.YELLOW}│{Fore.GREEN}░░░{Fore.YELLOW}│{Fore.GREEN}░{Fore.YELLOW}▌{Fore.GREEN}░{Fore.YELLOW}▌{Fore.GREEN}░░░░░░{Fore.YELLOW}│{Fore.GREEN}░{Fore.YELLOW}│{Fore.GREEN}░░░░░░
+          ░░░░░{Fore.YELLOW}──█┘{Fore.GREEN}░{Fore.RED}◎{Fore.GREEN}░{Fore.YELLOW}┴─█{Fore.GREEN}░{Fore.YELLOW}█{Fore.GREEN}░░░░░░{Fore.YELLOW}█─┴─{Fore.GREEN}░░░░░░
+         ░░{Fore.YELLOW}└█{Fore.GREEN}░░░ {Fore.YELLOW}█ {Fore.GREEN}░░░░░{Fore.YELLOW}▀██─┘┌┘{Fore.GREEN}░{Fore.YELLOW}█┘{Fore.GREEN}░░░░░{Fore.YELLOW}▄┴{Fore.GREEN}░
+         {Fore.YELLOW}└───█▄▄▄▄█{Fore.GREEN}░{Fore.YELLOW}┬┘{Fore.GREEN}░░░{Fore.YELLOW}██──┘{Fore.GREEN}░░{Fore.YELLOW}█{Fore.GREEN}░░{Fore.YELLOW}▄▄▄█
+                  {Fore.YELLOW}█─┘{Fore.GREEN}░░░░{Fore.YELLOW}██▄{Fore.GREEN}░░░{Fore.YELLOW}█  █  
+                   {Fore.YELLOW}▀▀▀▀▀████▀▀▀ ▀▀ 
+                        ████            └─┤  {Fore.GREEN}mr-alarcon{Fore.YELLOW}
                         ████     ┌─┘      └──┼─────┬────┴
-                        █████────┴──┤ Treefy │     ◎
+                        █████────┴──┤ {Fore.GREEN}Treefy{Fore.YELLOW} │     {Fore.RED}◎{Fore.YELLOW}
                         ████        └────────┴─ 
                         ████
                       ▒▒████▒▒
                ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓""")
+
+
+create_main_banner()
