2230e844c3283431d3a1aec75f4078c859801135
mr-alarcon <marcosdavidalarcon06@gmail.com>
2025-08-21 12:12:03 -0400

    Modify local_clone module to save binary files correctly

--- core/local_clone_tree.py
+++ core/local_clone_tree.py
@@ -4,6 +4,17 @@ import requests
 from core.extensions_files import create_extensions_dict
 from core.get_url_file import get_url_files
 
+
+binary_requierd_exts = (
+    "jpg", "jpeg", "png", "gif", "svg", "webp", "ico",
+    "woff", "woff2", "ttf", "eot",
+    "mp4", "webm", "ogg", "mp3", "wav", "mov",
+    "zip", "rar",
+    "exe", "bin",
+    "pem", "key", "crt", "p12", "pfx",
+    "pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx"
+)
+
 def create_local_clone_tree(absolute_urls, tree, base_path, print_urls=True):
     global url_files
     
@@ -21,12 +32,18 @@ def create_local_clone_tree(absolute_urls, tree, base_path, print_urls=True):
             current_path.parent.mkdir(parents=True, exist_ok=True)
             
             if key in url_files:
+
                 response = requests.get(url_files[key])
-                
-                with current_path.open("w", encoding="utf-8") as file:
-                    file.write(response.text)
-                    file.close()
-                    del file
+
+                if key.split("?")[0].endswith(binary_requierd_exts):
+                    with current_path.open("wb") as file:
+                        file.write(response.content)
+
+                else:            
+                    with current_path.open("w", encoding="utf-8") as file:
+                        file.write(response.text)
+                        file.close()
+                        del file
 
         else:
             current_path.mkdir(parents=True, exist_ok=True)
