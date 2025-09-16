a733925ea6929c306835b1f7259270e08bd4a335
mr-alarcon <marcosdavidalarcon06@gmail.com>
2025-09-16 02:25:04 -0400

    Fix handle empty directories by returning [] instead of None

--- core/directory_tree.py
+++ core/directory_tree.py
@@ -22,18 +22,21 @@ Functions:
 """
 
 
-# Builds a nested dictionary structure from a list of URL segments.
 def add_segments_to_tree(current_tree, segments):
+    if not segments:
+        return
+
     for segmt in segments:
         if segmt not in current_tree:
             current_tree[segmt] = {}
 
         current_tree = current_tree[segmt]
+        
 
 # Splits a URL into path segments, ignoring fragments (#).
 def url_segments(url):
     if url.startswith("#"):
-        return
+        return []
     else:
         url_seg = url.strip("/").split("/")
         return url_seg
