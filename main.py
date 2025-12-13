edf61e4f717647744ea64c3f8da3f67e8042d218
mr-alarcon <marcosdavidalarcon06@gmail.com>
2025-12-13 13:17:17 -0500

    feat: add CLI argument parser builder

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import argparse
+
+def build_parser():        
+    parser = argparse.ArgumentParser(description="Treefy by @mr-alarcon")
+
+    parser_arguments = parser.add_argument_group("Flags")
+    parser_arguments.add_argument("-u", "--url", metavar="URL", required=True, help="Target URL")
+    parser_arguments.add_argument("-t", "--tree", action="store_true", help="Show directory tree")
+
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+def main():
+    arguments = build_parser()
+
+
 if __name__ == "__main__":
-    pass
+    main()
